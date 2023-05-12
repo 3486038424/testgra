a54d8821b3fe4ec3174a72eface25d4ec47d481a
lapus <a3486038424@outlook.com>
2023-05-12 19:13:32 +0800

    add test tgra_win

--- Project2/源.cpp
+++ Project2/源.cpp
@@ -1,7 +1,9 @@
-#include<Windows.h>
+#include"tgra_win.h"
 #include <time.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	tgra_win* l = new tgra_win((LPSTR)"╣з0ап", 1400, 800, hInstance, hPrevInstance, lpCmdLine, nShowCmd);
+	l->loop();
 	return 0;
 }
 
