978b91bb3ef66daadf8d40622b5f2c2b614abe97
世源 马 <a3486038424@outlook.com>
2023-09-08 13:09:29 +0800

    qw

--- Project2/tg_vec2d.h
+++ Project2/tg_vec2d.h
@@ -5,6 +5,7 @@ class tg_vec2d
 public:
 	int x;
 	int y;
+	float z;
 	tg_vec2d(float x1 = 0, float x2 = 0) :x(x1), y(x2) {  };
 	tg_vec2d(float x1, float x2,float x3) :x(x1/x3), y(x2/x3) {  };
 	tg_vec2d(tg_vec3d tg) :x(tg.x / tg.z*100), y(tg.y / tg.z*100) {};
